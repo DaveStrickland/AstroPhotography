eee914974696bb1cc8046e9b341fbaf78941978c
Dave Strickland <dave.strickland@gmail.com>
2019-08-27 22:23:05 -0400

    File type detection in file_writer

--- src/AstroPhotography/api/grey.py
+++ src/AstroPhotography/api/grey.py
@@ -12,7 +12,7 @@ def main(rawfile, output, black, whitebalance):
     :param output: File prefix for output images. Output image 
       file names will consist of the prefix followed by '_r.png', 
       '_g1.png', '_b.png' and '_g2.png'.
-    :param whitebalance: Whitebalance to use when converting R, G and B
+    :param whitebalance: Whitebalance method to use when converting R, G and B
       channels to monochrome
     :param black: If true the camera black levels will be subtracted
       from the channel data.
@@ -21,23 +21,7 @@ def main(rawfile, output, black, whitebalance):
     
     # Read and process the file.
     rawconv = RawConv(rawfile)
-    r_im, g1_im, b_im, g2_im = rawconv.split(subtract_black=black)
-    
-    r_fname  = output + '_r.png'
-    g1_fname = output + '_g1.png'
-    b_fname  = output + '_b.png'
-    g2_fname = output + '_g2.png'
-    
-    logger.debug("Wrote R image to {:s}".format(r_fname))
-    imageio.imsave(r_fname, r_im)
-    
-    logger.debug("Wrote G1 image to {:s}".format(g1_fname))
-    imageio.imsave(g1_fname, g1_im)
-    
-    logger.debug("Wrote B image to {:s}".format(b_fname))
-    imageio.imsave(b_fname, b_im)
-    
-    logger.debug("Wrote G2 image to {:s}".format(g1_fname))
-    imageio.imsave(g2_fname, g2_im)
-
+    wb_vals = rawconv.get_whitebalance(whitebalance)
+    grey_im = rawconv.grey(subtract_black=black, wb_list=wb_vals)
+    file_writer(grey_im, output)
     return 
--- src/AstroPhotography/core/file_writer.py
+++ src/AstroPhotography/core/file_writer.py
@@ -8,20 +8,52 @@ import time
 import os.path
 
 def file_writer(data_array, out_file):
+    """Abstracts away details of writing an image to a graphics
+       file format or astronomical FITS file.
+       
+    :param data_array: 2-D (monochrome) or 3-D (rgb) ndarray of
+      data values. Usually but not always of dtype.uint16
+    :param out_file: File path/name for output file.
     """
-    """
+    
+    ftype = determine_file_type(out_file)
+    
+    # Image properties.
+    ndim  = data_array.ndim
     nrows = data_array.shape[0]
     ncols = data_array.shape[1]
+    nlayers = 1
+    if ndim == 3:
+        nlayers = data_array.shape[2]
+    elif ndim == 1 or ndim > 3:
+        err_str = 'Error: file_writer cannot handle {}-dimension arrays.'.format(ndim)
+        logger.error(err_str)
+        return # maybe throw?
+        
     dtype = data_array.dtype
     
-    logger.info('Writing {}x{} {} image to {:s}'.format(nrows,
-        ncols,
-        dtype,
-        out_file))
+    if ndim == 2:
+        info_str = 'Writing {}x{} {} image to {:s}'.format(nrows,
+            ncols,
+            dtype,
+            out_file)
+    elif ndim == 3:
+        info_str = 'Writing {}x{}x{} {} image to {:s}'.format(nrows,
+            ncols,
+            ndim,
+            dtype,
+            out_file)
+    logger.info(info_str)
         
     # TODO make sure write is succesful.
     t_start = time.perf_counter()
-    imageio.imsave(out_file, data_array)
+    if ftype == 'graphics':
+        imageio.imsave(out_file, data_array)
+    elif ftype == 'fits':
+        print('fits not yet implemented')
+        return
+    else:
+        raise RuntimeError('Could not determine type for output file {}'.format(out_file))
     t_end = time.perf_counter()
     
     io_size = os.path.getsize(out_file) / 1e6 # size in MB
@@ -32,3 +64,28 @@ def file_writer(data_array, out_file):
         io_rate))
     return
     
+def determine_file_type(fname):
+    """Determine if file is a common graphics format or FITS.
+    """
+    
+    # Graphics file formats, delegated to imageio
+    graphics_list = ['.tif', '.tiff', '.jpg', '.jp2', 'jpeg', '.png', '.gif']
+    
+    # FITS, delegate to astropy
+    fits_list = ['.fits', '.ftz', '.fit', '.fits.gz'] 
+    
+    # want file extension, e.g .png, .fits but also .fits.gz
+    root, ext = os.path.splitext(fname)
+    if '.gz' in ext:
+        root, ext2 = os.path.splitext(root)
+        ext = ext2 + ext
+    ext = ext.lower()
+        
+    if ext in fits_list:
+        ftype = 'fits'
+    elif ext in graphics_list:
+        ftype = 'graphics'
+    else:    
+        ftype = 'unknown'
+    
+    return ftype
