51d90ad25448804cc2b3294172da1cbcd6bceb2d
Dave Strickland <dave.strickland@gmail.com>
2020-08-05 08:52:14 -0400

    Added ap_find_stars.py to find stars in FITS images

---
+++ src/AstroPhotography/scripts/ap_find_stars.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  ap_find_stars.py
+#  
+#  Copyright 2020 Dave Strickland <dave.strickland@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+# 2020-08-05 dks : Initial coding
+
+import argparse
+import sys
+import logging
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+from astropy.io import fits
+from astropy.visualization import SqrtStretch
+from astropy.visualization import AsinhStretch
+from astropy.visualization.mpl_normalize import ImageNormalize
+from astropy.stats import sigma_clipped_stats
+
+from photutils import make_source_mask  
+from photutils import DAOStarFinder
+from photutils import CircularAperture
+from photutils import aperture_photometry
+
+def command_line_opts(argv):
+    """ Parse command line arguments.
+
+    :param argv: argument list to parse
+    """
+    parser = argparse.ArgumentParser(prog='ap_find_stars',
+        description='Find stars in a FITS image using photutils')
+        
+    # Required
+    parser.add_argument('fits_image',
+        help='Input FITS image file name to process.')
+    parser.add_argument('source_list',
+        help='File name for output FITS table of detected sources.')
+        
+    # Optional
+    parser.add_argument('-d', '--ds9',
+        default=None,
+        metavar='ds9.reg',
+        help='Name for optional ds9-format region file.')
+    parser.add_argument('-l', '--loglevel', 
+        default='INFO',
+        help='Logging message level. Default: INFO')
+    parser.add_argument('-e','--fits_extension',
+        default=0,
+        type=int,
+        metavar='EXT_NUM',
+        help='FITS extension number to read from. Default=0 (Primary)')
+            
+    args = parser.parse_args(argv)
+    return args
+
+def initialize_logger(loglevel):
+    """Initialize and return the logger
+    """
+    
+    logger = logging.getLogger(__name__)
+    
+    # Check that the input log level is legal
+    numeric_level = getattr(logging, loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: {}'.format(loglevel))
+    logger.setLevel(numeric_level)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(numeric_level)
+
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
+    return logger
+
+def read_fits(image_filename, image_extension):
+    """Read a single extension's data and header from a FITS file
+    """
+
+    logger = logging.getLogger(__name__)
+    logger.info('Loading extension {} of FITS file {}'.format(image_extension, image_filename))
+        
+    # open() parameters that can be important.
+    # Default values used here.
+    # See https://docs.astropy.org/en/stable/io/fits/api/files.html#astropy.io.fits.open
+    uint_handling = True
+    image_scaling = False
+        
+    with fits.open(image_filename, 
+        uint=uint_handling, 
+        do_not_scale_image_data=image_scaling) as hdu_list:
+        ext_hdr  = hdu_list[image_extension].header
+        ext_data = hdu_list[image_extension].data
+        
+    ndim     = ext_hdr['NAXIS']
+    cols     = ext_hdr['NAXIS1']
+    rows     = ext_hdr['NAXIS2']
+    bitpix   = ext_hdr['BITPIX']
+    bzero    = ext_hdr['BZERO']
+    bscale   = ext_hdr['BSCALE']
+    info_str = '{}-D BITPIX={} image with {} columns, {} rows, BSCALE={}, BZERO={}'.format(ndim, bitpix, cols, rows, bscale, bzero)
+    
+    if ndim == 3:
+        layers = ext_hdr['NAXIS3']
+        info_str = '{}-D BITPIX={} image with {} columns, {} rows, {} layers, BSCALE={}, BZERO={}'.format(ndim, bitpix, cols, rows, layers, bscale, bzero)
+        
+    logger.debug(info_str)
+        
+    return ext_data, ext_hdr
+
+def main(args=None):
+    p_args    = command_line_opts(args)
+    logger    = initialize_logger(p_args.loglevel)    
+    data, hdr = read_fits(p_args.fits_image, p_args.fits_extension)
+    
+    sqrt_norm  = ImageNormalize(stretch=SqrtStretch())
+    asinh_norm = ImageNormalize(stretch=AsinhStretch())
+    
+    # TODO should replace plt use with proper fig, ax etc...
+    ##plt.imshow(data, norm=sqrt_norm, origin='lower')
+    ##plt.show()
+
+    mean, median, std = sigma_clipped_stats(data, sigma=3.0)
+    print('Raw image stats:     ', (mean, median, std))  
+
+    mask = make_source_mask(data, nsigma=2, npixels=5, dilate_size=11)
+    mean, median, std = sigma_clipped_stats(data, sigma=3.0, mask=mask)
+    print('Source masked stats: ', (mean, median, std))
+    
+    # TODO better estimate of FWHM, sigma to use
+    daofind = DAOStarFinder(fwhm=3.0, threshold=7.*std)  
+    sources = daofind(data - median)  
+    for col in sources.colnames:  
+        sources[col].info.format = '%.8g'  # for consistent table output
+    print('Initial source list:\n', sources)
+    
+    # TODO better estimate of aperture to use
+    positions = np.transpose((sources['xcentroid'], sources['ycentroid']))
+    apertures = CircularAperture(positions, r=4.)
+
+    plt.imshow(data, cmap='Greys', origin='lower', norm=asinh_norm)
+    apertures.plot(color='red', lw=1.5, alpha=0.5)
+    
+    # Use initial source list for aperture photometry
+    phot_table = aperture_photometry(data -median, apertures)
+    phot_table['aperture_sum'].info.format = '%.4f'  # for consistent table output
+    print(phot_table)
+    
+    exposure = float(hdr['EXPOSURE'])
+    phot_table['adu_per_sec'] = phot_table['aperture_sum'] / exposure
+    phot_table['magnitude']   = -2.5 * np.log10(phot_table['adu_per_sec'])
+    
+    phot_table['adu_per_sec'].info.format = '%.4f'
+    phot_table['magnitude'].info.format   = '%.4f'
+    phot_table['xcenter'].info.format     = '%.2f'
+    phot_table['ycenter'].info.format     = '%.2f'
+
+    phot_table.sort(['adu_per_sec', 'xcenter', 'ycenter'], reverse=True)
+    print(phot_table)
+
+    # TODO filter brightest
+    # TODO add 1 to get FITS like pixel indices
+    # Generate header
+    
+    plt.show()
+    logger.info('Writing source list to FITS binary table {}'.format(p_args.source_list))
+    
+    # Simplistic write with no added header
+    phot_table.write(p_args.source_list)
+    return 0
+
+if __name__ == '__main__':
+    try:
+        status = main()
+    except:
+        logging.getLogger(__name__).critical("Shutting down due to fatal error")
+        raise  # print stack trace
+    else:
+        raise SystemExit(status)
