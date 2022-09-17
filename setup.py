6166d962eba9309b8f777f6968131aec640f0b3f
Dave Strickland <dave.strickland@gmail.com>
2022-09-17 07:03:42 -0400

    WIP on updated setup.py

--- setup.py
+++ setup.py
@@ -1,56 +1,50 @@
-""" Setup script for the AstroPhotography application.
+#!/usr/bin/env python
 
 """
-from os import walk
-from pathlib import Path
-from setuptools import find_packages
-from setuptools import setup
+Setup script for the AstroPhotography application.
+"""
 
+from setuptools import setup, find_packages
 
-_config = {
-    "name": "AstroPhotography",
-    "url": "https://github.com/DaveStrickland/AstroPhotography",
-    "author": "Dave Strickland",
-    "author_email": "dave.strickland@gmail.com",
-    "package_dir": {"": "src"},
-    "packages": find_packages("src"),
-    "entry_points": {
-        "console_scripts": ("dksraw = AstroPhotography.cli:main",),
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('RELEASE_NOTES.md') as history_file:
+    history = history_file.read()
+
+requirements = [ ]
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="Dave Strickland",
+    author_email='dave.strickland@gmail.com',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Python workflow f for processing astrophotographic images from amateur telescopes and/or digital cameras",
+    entry_points={
+        'console_scripts': [
+            'dksraw=AstroPhotography.cli:main',
+        ],
     },
-    "data_files": ("etc/",),
-}
-
-
-def main() -> int:
-    """ Execute the setup command.
-
-    """
-    def data_files(*paths):
-        """ Expand path contents for the `data_files` config variable.  """
-        for path in map(Path, paths):
-            if path.is_dir():
-                for root, _, files in walk(str(path)):
-                    yield root, tuple(str(Path(root, name)) for name in files)
-            else:
-                yield str(path.parent), (str(path),)
-        return
-
-    def version():
-        """ Get the local package version. """
-        namespace = {}
-        path = Path("src", _config["name"], "__version__.py")
-        exec(path.read_text(), namespace)
-        return namespace["__version__"]
-
-    _config.update({
-        "data_files": list(data_files(*_config["data_files"])),
-        "version": version(),
-    })
-    setup(**_config)
-    return 0
-
-
-# Make the script executable.
-
-if __name__ == "__main__":
-    raise SystemExit(main())
+    install_requires=requirements,
+    license="GNU General Public License v3",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='AstroPhotography',
+    name='AstroPhotography',
+    packages=find_packages(include=['AstroPhotography', 'AstroPhotography.*']),
+    test_suite='test',
+    tests_require=test_requirements,
+    url='https://github.com/DaveStrickland/AstroPhotography',
+    version='0.5.0',
+    zip_safe=False,
+)
